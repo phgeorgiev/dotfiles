f9d92725098534005dfc66501d26d8e877d10311
Philip Georgiev <p.georgiev359@gmail.com>
2022-04-05 23:28:01 +0300

    Install powerlevel10k theme

--- install.sh
+++ install.sh
@@ -64,6 +64,9 @@ fi
 #Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# Install powerlevel10k theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+
 # Install symlinks
 for file in "$dots_dir"/*; do
 	fileName="$(basename "$file" suffix)"
