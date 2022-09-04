344eafc24a37167fd1386c5fe44fd4f181fd1e07
Philip Georgiev <p.georgiev359@gmail.com>
2022-09-04 13:17:02 +0300

    Install vim-plugin

--- install.sh
+++ install.sh
@@ -70,7 +70,7 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 # Install powerlevel10k theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 
-# Install symlinks
+# Symlinks dotfiles
 for file in "$dots_dir"/*; do
 	fileName="$(basename "$file" suffix)"
 	$dry_run ln -s "$file" "$target_dir/.$fileName"
