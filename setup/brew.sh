927f47a42d21fd8451e9e50bc3feb2682710393c
Philip Georgiev <p.georgiev359@gmail.com>
2022-10-24 23:00:52 +0300

    Install openjdk

--- setup/brew.sh
+++ setup/brew.sh
@@ -30,6 +30,7 @@ $dry_run brew install zsh-syntax-highlighting
 $dry_run brew install diff-so-fancy
 $dry_run brew install git-friendly/git-friendly/git-friendly
 $dry_run brew install go@1.19
+$dry_run brew install openjdk@19
 
 echo
 echo "${green}Success! Packages installed.${normal}"
