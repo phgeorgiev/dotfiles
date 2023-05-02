4f8fdf451a0999a5b7d9dbf03c94e7d270e9a8ee
Philip Georgiev <p.georgiev359@gmail.com>
2023-05-02 22:22:13 +0200

    Install logi-options-plus

--- setup/brew.sh
+++ setup/brew.sh
@@ -57,6 +57,7 @@ $dry_run brew install --cask --appdir="/Applications" discord
 $dry_run brew install --cask --appdir="/Applications" obsidian
 $dry_run brew install --cask --appdir="/Applications" microsoft-teams
 $dry_run brew install --cask --appdir="/Applications" spotify
+$dry_run brew install --cask --appdir="/Applications" logi-options-plus
 
 echo
 echo "${green}Success! Cask packages installed.${normal}"
