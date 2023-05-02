c3518af933c1bfb494319985c0a7b7773e93d55b
Philip Georgiev <p.georgiev359@gmail.com>
2023-05-02 21:53:27 +0200

    Install phpstorm

--- setup/brew.sh
+++ setup/brew.sh
@@ -47,6 +47,7 @@ $dry_run brew install --cask --appdir="/Applications" insomnia
 $dry_run brew install --cask --appdir="/Applications" sequel-ace
 $dry_run brew install --cask --appdir="/Applications" visual-studio-code
 $dry_run brew install --cask --appdir="/Applications" intellij-idea
+$dry_run brew install --cask --appdir="/Applications" phpstom
 $dry_run brew install --cask --appdir="/Applications" iterm2
 $dry_run brew install --cask --appdir="/Applications" 1password
 $dry_run brew install --cask --appdir="/Applications" figma
