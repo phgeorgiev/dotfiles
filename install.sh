fd527d0903e33ba91103609ef8708b7d6d6dfe67
Philip Georgiev <p.georgiev359@gmail.com>
2024-08-25 20:20:36 +0200

    Don't install packages if they are already installed

--- install.sh
+++ install.sh
@@ -28,87 +28,89 @@ function usage {
 is_dry_run=false
 dry_run=
 install=false
-target_dir="$HOME"
 while (($#)); do
 	case "$1" in
-		--dry-run)
-			is_dry_run=true;
-			dry_run=dry;
-			;;
-		--help)
-			usage;
-			exit 0;
-			;;
-		--install)
-			install=true
-			;;
-		*)
-			[ "$1" = '--' ] && shift
-			if (($#)); then
-				# There are unexpected arguments.
-				usage
-				exit 1
-			fi
+	--dry-run)
+		is_dry_run=true
+		dry_run=dry
+		;;
+	--help)
+		usage
+		exit 0
+		;;
+	--install)
+		install=true
+		;;
+	*)
+		[ "$1" = '--' ] && shift
+		if (($#)); then
+			# There are unexpected arguments.
+			usage
+			exit 1
+		fi
+		;;
 	esac
 	shift
 done
 
-
 $is_dry_run && $dry_run "Dry run mode. Not actually executing anything."
 
-source_dir="$(cd "$(dirname "$0")" > /dev/null; pwd)"
-dots_dir="$source_dir/dots"
+source_dir="$(
+	cd "$(dirname "$0")" >/dev/null || exit
+	pwd
+)"
 
 # Ask for the administrator password upfront
-if [ $is_dry_run == false ] ; then
-    echo 'Asking for administrator password upfront\n' && sudo -v
+if [ $is_dry_run == false ]; then
+	echo 'Asking for administrator password upfront' && sudo -v
 fi
 
 # Install brew
-if test ! $(which brew); then
-    title "Installing Homebrew..."
-    $dry_run /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    if test ! $(which brew); then
-        echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
-        eval "$(/opt/homebrew/bin/brew shellenv)"
-    fi
+if test ! "$(which brew)"; then
+	title "Installing Homebrew..."
+	$dry_run /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	if test ! "$(which brew)"; then
+		echo eval "$(/opt/homebrew/bin/brew shellenv)" >>~/.zprofile
+		eval "$(/opt/homebrew/bin/brew shellenv)"
+	fi
 fi
 
-if [ $install == true ] ; then
+if [ $install == true ]; then
 	. "$source_dir/setup/brew.sh"
 
 	echo "For the system Java wrappers to find this JDK, symlink it with"
 	sudo ln -sfn /opt/homebrew/opt/openjdk@17/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
 
 	title "Install Rust"
-	if [ $is_dry_run == false ] ; then
+	if [ $is_dry_run == false ]; then
 		curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 	fi
 fi
 
-title "Install Oh My Zsh"
-if [ $is_dry_run == false ] ; then
-	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+	title "Install Oh My Zsh"
+	$dry_run sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
-title "Install powerlevel10k theme"
-if [ $is_dry_run == false ] ; then
-	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+if [ ! -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
+	title "Install powerlevel10k theme"
+	$dry_run git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 fi
 
-title "Install TPM"
-if [ $is_dry_run == false ] ; then
-	git clone --depth=1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+	title "Install TPM"
+	$dry_run git clone --depth=1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
-title "Install Composer"
-if [ $is_dry_run == false ] ; then
-	php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-	php -r "if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-	php composer-setup.php
-	php -r "unlink('composer-setup.php');"
+if test ! "$(which composer)"; then
+	title "Install Composer"
 
-	sudo mv composer.phar /usr/local/bin/composer
+	$dry_run php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+	$dry_run php -r "if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+	$dry_run php composer-setup.php
+	$dry_run php -r "unlink('composer-setup.php');"
+
+	$dry_run sudo mv composer.phar /usr/local/bin/composer
 
 	. "$source_dir/setup/composer.sh"
 fi
