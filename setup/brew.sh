807a38de393be7f074f718c366167c6b9f53f78a
Filip Georgiev <filip.georgiev@hey.com>
2024-09-02 19:09:01 +0200

    Update brew packags

--- setup/brew.sh
+++ setup/brew.sh
@@ -9,6 +9,9 @@ $dry_run brew upgrade --all
 $dry_run brew tap homebrew/cask-fonts
 
 title "Installing packages..."
+$dry_run brew install zsh
+$dry_run brew install zsh-syntax-highlighting
+$dry_run brew install tmux
 $dry_run brew install yq
 $dry_run brew install tree
 $dry_run brew install neofetch
@@ -17,18 +20,15 @@ $dry_run brew install wget
 $dry_run brew install git
 $dry_run brew install git-extras
 $dry_run brew install gh
-$dry_run brew install gitui
+$dry_run brew install lazygit
 $dry_run brew install node
+$dry_run brew install yarn
 $dry_run brew install wifi-password
-$dry_run brew install zsh
-$dry_run brew install zsh-syntax-highlighting
 $dry_run brew install diff-so-fancy
 $dry_run brew install git-friendly/git-friendly/git-friendly
 $dry_run brew install go
 $dry_run brew install openjdk@21
 $dry_run brew install maven
-$dry_run brew install yarn
-$dry_run brew install tmux
 $dry_run brew install php
 $dry_run brew install symfony-cli/tap/symfony-cli
 $dry_run brew install kubernetes-cli
@@ -42,33 +42,28 @@ echo "${green}Success! Packages installed.${normal}"
 sleep 1
 
 title "Installing cask packages..."
+$dry_run brew install --cask 1password
+$dry_run brew install --cask arc
+$dry_run brew install --cask raycast
+$dry_run brew install --cask slack
+$dry_run brew install --cask iterm2
+$dry_run brew install --cask font-jetbrains-mono-nerd-font
+$dry_run brew install --cask docker
 $dry_run brew install --cask quicklook-json
 $dry_run brew install --cask syntax-highlight
 $dry_run brew install --cask qlmarkdown
 $dry_run brew install --cask quicklook-csv
 $dry_run brew install --cask betterzip
 $dry_run brew install --cask istat-menus
-$dry_run brew install --cask docker
-$dry_run brew install --cask slack
 $dry_run brew install --cask insomnia
-$dry_run brew install --cask sequel-ace
 $dry_run brew install --cask visual-studio-code
 $dry_run brew install --cask intellij-idea
 $dry_run brew install --cask phpstorm
-$dry_run brew install --cask webstorm
-$dry_run brew install --cask iterm2
-$dry_run brew install --cask 1password
 $dry_run brew install --cask figma
 $dry_run brew install --cask firefox
-$dry_run brew install --cask sourcetree
 $dry_run brew install --cask discord
 $dry_run brew install --cask obsidian
-$dry_run brew install --cask microsoft-teams
-$dry_run brew install --cask spotify
 $dry_run brew install --cask logi-options-plus
-$dry_run brew install --cask arc
-$dry_run brew install --cask font-jetbrains-mono-nerd-font
-$dry_run brew install --cask raycast
 
 echo
 echo "${green}Success! Cask packages installed.${normal}"
