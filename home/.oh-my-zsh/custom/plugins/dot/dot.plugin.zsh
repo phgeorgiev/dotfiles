240cad0dec1eb537e22ed193d87fa5a06247fc03
Filip Georgiev <filip.georgiev@hey.com>
2025-07-26 19:04:23 +0200

    fix: autoloading the completion

--- home/.oh-my-zsh/custom/plugins/dot/dot.plugin.zsh
+++ home/.oh-my-zsh/custom/plugins/dot/dot.plugin.zsh
@@ -2,12 +2,9 @@ if (( ! $+commands[dot] )); then
   return
 fi
 
-# If the completion file doesn't exist yet, we need to autoload it and
-# bind it to `dot`. Otherwise, compinit will have already done that.
-if [[ ! -f "$ZSH_CACHE_DIR/completions/_dot" ]]; then
-  typeset -g -A _comps
-  autoload -Uz _dot
-  _comps[dot]=_dot
-fi
+# Add the plugin's completions directory to fpath
+fpath=("${0:h}/completions" $fpath)
 
-command cp "${0:h}/completions/_dot" "$ZSH_CACHE_DIR/completions/_dot"
+# Load the completion function
+autoload -Uz _dot
+compdef _dot dot
